1691997b4b1eec58ca86ef8b012fb0c5bf160fde
Melissa Real <melissa.real@hotmail.com>
2018-09-03 16:01:46 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,10 +41,10 @@ def list_songs_by_artist
   #art = gets
   name = gets
   artist = Artist.find_by_name(name)
-  if artist == nil
-  else
-  artist.songs.sort_by {|song| song.name}.each_with_index {|element, index| puts "#{index+1}. #{element.name} - #{element.genre.name}"}
-  end
+  #if 
+    if !artist.nil
+      artist.songs.sort_by {|song| song.name}.each_with_index {|element, index| puts "#{index+1}. #{element.name} - #{element.genre.name}"}
+    end
   #martist = Artist.find_by_name(art)
   #  if !martist.nil?
   #    martist.list_songs
