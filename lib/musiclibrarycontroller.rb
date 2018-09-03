3155fdf4a3c457a769a164a55293e533f265ad0d
Melissa Real <melissa.real@hotmail.com>
2018-09-03 17:07:11 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -42,8 +42,8 @@ attr_accessor :path, :musicimporter
  end
 
 def list_songs
-  Song.all.sort_by{|s| s.name}.each_with_index {|a, i| puts "#{i+1}. #{a.artist.name} - #{a.name} - #{a.genre.name}"}
-  binding.pry
+  Song.all.sort_by!{|s| s.name}.each_with_index {|a, i| puts "#{i+1}. #{a.artist.name} - #{a.name} - #{a.genre.name}"}
+
 end
 
 def list_artists
